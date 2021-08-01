b47743a720e6dc2a5fcfdadad8e00c240634f24d
Kevin G <kjangamboa@hotmail.com>
2021-08-01 17:07:19 +0800

    side menu animation and function

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
+import "./app.scss";
+import { useState } from 'react';
+
+//Components
 import Topbar from './components/topbar/Topbar';
 import Intro from './components/intro/Intro';
 import Portfolio from './components/portfolio/Portfolio';
 import Works from './components/works/Works';
 import Testimonials from './components/testimonials/Testimonials';
 import Contact from './components/contact/Contact';
-import "./app.scss";
-import { useState } from 'react';
-
+import Menu from './components/menu/Menu';
 
 function App() {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <div className="app menu">
+    <div className="app">
       <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       <div className='sections'>
-        <div className='sideMenu'>
-          <h1>testing</h1>
-        </div>
         <Intro />
         <Portfolio />
         <Works />
